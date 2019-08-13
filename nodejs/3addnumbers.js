e77e5716832a94d93565e537fa02cd23b796c618
angelrosecd <eddykuttan18@gmail.com>
2019-08-13 15:01:28 +0530

    custom

---
+++ nodejs/3addnumbers.js
@@ -0,0 +1,11 @@
+const op=require('./3operation')
+var x=10
+var y=15
+var z=op.add(x,y)
+var s=op.sub(x,y)
+var k=op.mul(x,y)
+var j=op.div(x,y)
+console.log(z)
+console.log(s)
+console.log(k)
+console.log(j)
